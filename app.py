cd936bf8000c306be575c38a8bba0c98e2ddc1f9
Kevin Li <jshiansongli@gmail.com>
2020-08-24 22:27:58 -0400

    gui fix?

--- app.py
+++ app.py
@@ -9,17 +9,18 @@ pd.set_option("display.max_rows", None, "display.max_columns", None)
 data=pd.read_excel('MPVDatasetDownload.xlsx',index_col=0)
 
 # Set up charts
-fig, axs = plt.subplots()
+fig, axs = plt.subplots(4,4)
 cmap = plt.get_cmap('Spectral')
 colors = [cmap(i) for i in np.linspace(0, 1, 8)]
+stateColors = [cmap(i) for i in np.linspace(0, 1, 51)]
 
 # Titles
-#axs[0,0].set_title("Killings by Race")
-#axs[0,1].set_title("Killings by State")
-#axs[0,2].set_title("Killings by Gender")
-#axs[0,3].set_title("Killings over Time")
-#axs[1,0].set_title("Killings by Race Against Population per Million")
-#axs[1,1].set_title("Killings by Day")
+axs[0,0].set_title("Killings by Race")
+axs[0,1].set_title("Killings by State")
+axs[0,2].set_title("Killings by Gender")
+axs[0,3].set_title("Killings over Time")
+axs[1,0].set_title("Killings by Race Against Population per Million")
+axs[1,1].set_title("Killings by Day")
 
 print(data.head())
 
@@ -28,7 +29,7 @@ race_labels = data["Victim's race"].value_counts().index
 race_counts = data["Victim's race"].value_counts()
 
 # Make the charts
-#axs[0,0].pie(race_counts,labels=race_labels,autopct="%1.1f%%",colors=colors)
+axs[0,0].pie(race_counts,labels=race_labels,autopct="%1.1f%%",colors=colors)
 
 # Select White, Black, and Latino
 proprace_counts = race_counts[0:3].astype(float)
@@ -43,13 +44,13 @@ proprace_counts = proprace_counts.sort_values(ascending=False)
 print(proprace_counts)
 print(proprace_counts.index)
 
-#axs[1,0].bar(proprace_counts.index,proprace_counts, color=colors)
+axs[1,0].bar(proprace_counts.index,proprace_counts, color=colors)
 
 # Get state data
 state_labels = data["State"].value_counts().index
 state_counts = data["State"].value_counts()
 
-#axs[0,1].bar(state_labels,state_counts,color=colors)
+axs[0,1].bar(state_labels,state_counts,color=stateColors)
 
 # Gender data
 gender_labels = data["Victim's gender"].value_counts().index
@@ -60,14 +61,14 @@ total = float(sum(gender_counts))
 for x in range(len(gender_counts)):
     gender_percents.append(gender_counts[x]/total)
 
-#axs[0,2].bar(gender_labels,gender_percents)
+axs[0,2].bar(gender_labels,gender_percents)
 
 # Y/Y data
 time_count = data["Date of Incident (month/day/year)"].groupby(data["Date of Incident (month/day/year)"].dt.year).agg('count')
 
 time_count=time_count[0:-1]
 
-#axs[0,3].plot(time_count, color="red")
+axs[0,3].plot(time_count, color="red")
 
 # D/D data
 day_count = data["Date of Incident (month/day/year)"].groupby(data["Date of Incident (month/day/year)"]).agg('count')
@@ -76,7 +77,7 @@ print(day_count)
 print(day_count.index)
 print(day_count.values)
 
-#axs[1,1].plot(day_count)
+axs[1,1].plot(day_count)
 
 #day_count.drop()
 
@@ -117,6 +118,6 @@ print(data["Criminal Charges?"])
 print(data["Criminal Charges?"].count())
 account_labels = data["Criminal Charges?"].value_counts().index
 account_counts = data["Criminal Charges?"].value_counts()
-axs.pie(account_counts,labels=account_labels,autopct="%1.1f%%",colors=colors)
+axs[1,2].pie(account_counts,labels=account_labels,autopct="%1.1f%%",colors=colors)
 
 plt.show()
